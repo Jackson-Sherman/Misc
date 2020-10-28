22e63f2fcf36606e2046d75b461b3130d342e71b
jajsherm <jajsherm@iu.edu>
2020-10-28 16:22:19 -0400

    I made matrix a class :) yay!

--- python/matrix.py
+++ python/matrix.py
@@ -2,279 +2,201 @@ import random
 from math import floor
 import re
 
-mat_in = [
-    [-5,25,-125],
-    [-4,16,-64],
-    [-3,9,-27],
-    [-2,4,-8],
-    [-1,1,-1],
-    [0,0,0],
-    [1,1,1],
-    [2,4,8],
-    [3,9,27],
-    [4,16,64],
-    [5,25,125]
-]
 
-mat_test = [
-    [-34745,-1737,87318,7,],
-    [296,74,605,-2,],
-    [-56093,-5008,-73,6,],
-    [-27,-459,0,-829,],
-    [-79867,5215,-760,-7,],
-    [-5096,-2,6445,22,],
-    [5822,-96,2,8,],
-]
-mat_test1 = [
-    [1,22,1,22,],
-    [22,4444,22,4444,],
-    [1,22,-1,-22,],
-    [22,4444,-22,-4444,],
-]
-
-def dim(mat):
-    """
-    returns a tuple of len=2 containing the dimensions of the matrix mat. (#(rows),#(cols))
-    """
-    rows = len(mat)
-    for each_row in mat:
-        assert len(each_row) == len(mat[0]), "the matrix is not a rectangle"
-    return (len(mat), len(mat[0]))
-def toString(mat, align = "center"):
-    """
-    prints mat in an apealing way
-    """
-    if type(mat) == type(None):
-        return None
-    height = len(mat)
-    width = len(mat[0])
-    #makes a matrix of strings of the values of the original mat
-    string_mat = []
-    for i in range(height):
-        string_mat.append([])
-        for j in range(width):
-            string_mat[len(string_mat) - 1].append(str(mat[i][j]))
-            #this_row += [str(element)]
-        #string_mat += [this_row]
-    
-    #makes a list of the max length of each column so they can be lined up well
-    #also makes a list of bools whether or not there are any negatives in the list
-    lengths = []
-    negatives = []
-    for col_index in range(width):
-
-        max_length = len(string_mat[0][col_index])
-        minus = bool(mat[0][col_index] < 0)
-
-        if width > 1:
-            for row_index in range(1, height):
-                string = string_mat[row_index][col_index]
-                length = len(string)
-
-                if max_length < length:
-                    max_length = length
-
-                if mat[row_index][col_index] < 0:
-                    minus = True
-
-        lengths += [max_length]
-        negatives += [minus]
-
-    def num2let(num):
-        num = int(num)
-        num %= 52
-        littlestring = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        out = littlestring[num]
-        return out
-
-    #uses the above list to actually line it up
-    for y in range(height):
-        for x in range(width):
-
-            if negatives[x] and (mat[y][x] >= 0) and len(string_mat[y][x]) > lengths[x]:
-                string_mat[y][x] = " " + string_mat[y][x]
-            
-            count = 0
-            while len(string_mat[y][x]) < lengths[x]:
-                if re.search("^\s*[rR]",align):
-                    string_mat[y][x] = " " + string_mat[y][x]
-
-                elif re.search("^\s*[lL]",align):
-                    string_mat[y][x] = string_mat[y][x] + " "
-
-                else:
-                    if count % 2 == 0:
-                        string_mat[y][x] = " " + string_mat[y][x]
-                    else:
-                        string_mat[y][x] = string_mat[y][x] + " "
-                
-
-                count += 1
-    
-    #makes a list of strings of each row of the matrix for printing
-    string_list = []
-    string = ""
-    for i, row in enumerate(string_mat):
-        if len(string_mat) == 1:
-            string += "("
-        elif i == 0:
-            string += "⎛"
-        elif i + 1 < len(string_mat):
-            string += "⎜"
-        else:
-            string += "⎝"
+class matrix:
+    def __init__(self, alist):
+        self.list = alist
+        for each_row in self.list:
+            assert len(each_row) == len(self.list[0]), "the matrix is not a rectangle"
         
-        for j, value in enumerate(row):
-            string += str(value)
-            string += " " * 2
-        string = string[:-2]
+        self.align = "center"
+        self.dim = (len(self.list), len(self.list[0]))
 
-        if len(string_mat) == 1:
-            string += ")"
-        elif i == 0:
-            string += "⎞\n"
-        elif i + 1 < len(string_mat):
-            string += "⎟\n"
-        else:
-            string += "⎠"
+    def __str__(self):
+        """
+        makes a string of self.list in an apealing way
+        """
+        #makes a matrix of strings of the values of the original mat
+        string_mat = []
+        for i in range(self.dim[0]):
+            string_mat.append([])
+            for j in range(self.dim[1]):
+                string_mat[len(string_mat) - 1].append(str(self.list[i][j]))
+                #this_row += [str(element)]
+            #string_mat += [this_row]
+        
+        #makes a list of the max length of each column so they can be lined up well
+        #also makes a list of bools whether or not there are any negatives in the list
+        lengths = []
+        negatives = []
+        for col_index in range(self.dim[1]):
 
-    return string
+            max_length = len(string_mat[0][col_index])
+            minus = bool(self.list[0][col_index] < 0)
 
-def print_mat(lemat, lalign="center"):
-    print(toString(lemat,lalign))
+            if self.dim[1] > 1:
+                for row_index in range(1, self.dim[0]):
+                    string = string_mat[row_index][col_index]
+                    length = len(string)
 
-def rotate(mat, how_many_times = 1):
-    """
-    rotates a matrix (arg1) a number of times (arg2):
-    """
-    how_many_times %= 4
-    matter = mat
-    for times in range(how_many_times):
-        new_mat = []
-        i = len(matter[0])
-        while i > 0:
-            i -= 1
+                    if max_length < length:
+                        max_length = length
 
-            row = []
-            for j in range(len(matter)):
-                row += [matter[j][i]]
-            new_mat += [row]
-        matter = new_mat
-    return matter
-    
-def reflect(mat, direction = 0):
-    def true_direction(dire):
-        if type(dire) == type(0) or type(dire) == type(0.5) or type(dire) == type(3 +2j) or type(dire) == type(True):
-            if dire:
-                return True
-            else:
-                return False
+                    if self.list[row_index][col_index] < 0:
+                        minus = True
 
-        if re.search("\s*^(0|[lLrR])",dire):
-            pass
-    if direction:
-        pass
+            lengths += [max_length]
+            negatives += [minus]
 
-def multiply(matA, matB=None):
-    def entire(row=0):
-        if row == len(matA):
-            return []
-        else:
-            def across(col=0):
-                if col == len(matB[0]):
-                    return []
-                else:
-                    def each(i=0):
-                        if i == len(matB):
-                            return 0
-                        else:
-                            return matA[row][i] * matB[i][col] + each(i + 1)
+        def num2let(num):
+            num = int(num)
+            num %= 52
+            littlestring = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            out = littlestring[num]
+            return out
 
-                    return [each()] + across(col + 1)
+        #uses the above list to actually line it up
+        for y in range(self.dim[0]):
+            for x in range(self.dim[1]):
 
-            return [across()] + entire(row + 1)
-
-    if matB == None:
-        matB = matA[1]
-        matA = matA[0]
-
-    if len(matA[0]) != len(matB):
-        return None
-    return entire()
-
-def add(matA, matB=None):
-    themats = (matA if matB == None else (matA, matB))
-    mats = themats
-    size = dim(mats[0])
-    for each_mat in mats:
-        assert size == dim(each_mat), "matrix dimension error"
-
-    out = []
-    for y in range(size[0]):
-        row = []
-        for x in range(size[1]):
+                if negatives[x] and (self.list[y][x] >= 0) and len(string_mat[y][x]) > lengths[x]:
+                    string_mat[y][x] = " " + string_mat[y][x]
                 
-            elem = 0
-            for i in range(len(mats)):
-                elem += mats[i][y][x]
-            row.append(elem)
-        out += [row]
-    return out
+                count = 0
+                while len(string_mat[y][x]) < lengths[x]:
+                    if re.search("^\s*[rR]",self.align):
+                        string_mat[y][x] = " " + string_mat[y][x]
 
-def transpose(mat):
-    if mat == [] or mat == [[]]:
-        return mat
+                    elif re.search("^\s*[lL]",self.align):
+                        string_mat[y][x] = string_mat[y][x] + " "
+
+                    else:
+                        if count % 2 == 0:
+                            string_mat[y][x] = " " + string_mat[y][x]
+                        else:
+                            string_mat[y][x] = string_mat[y][x] + " "
+                    
+
+                    count += 1
+        
+        #makes a list of strings of each row of the matrix for printing
+        string_list = []
+        string = ""
+        for i, row in enumerate(string_mat):
+            if len(string_mat) == 1:
+                string += "("
+            elif i == 0:
+                string += "⎛"
+            elif i + 1 < len(string_mat):
+                string += "⎜"
+            else:
+                string += "⎝"
+            
+            for j, value in enumerate(row):
+                string += str(value)
+                string += " " * 2
+            string = string[:-2]
+
+            if len(string_mat) == 1:
+                string += ")"
+            elif i == 0:
+                string += "⎞\n"
+            elif i + 1 < len(string_mat):
+                string += "⎟\n"
+            else:
+                string += "⎠"
+
+        return string
+
+    def __add__(self, other):
+        assert self.dim == other.dim, "matrix dimension error"
+
+        out = []
+        for y in range(self.dim[0]):
+            row = []
+            for x in range(self.dim[1]):
+                row.append(self.list[y][x] + other.list[y][x])
+            out += [row]
+        
+        return matrix(out)
     
-    outmat = []
-    for coli in range(len(mat[0])):
-        newrow = []
-        for rowi in range(len(mat)):
-            newrow += [mat[rowi][coli]]
-        outmat += [newrow]
-    return outmat
+    def __mul__(self, other):
+        assert self.dim[1] == other.dim[0], "width of first matrix is different from the height of the second"
+        def entire(row=0):
+            def across(col=0):
+                def each(i=0):
+                    if i == other.dim[0]:
+                        return 0
+                    return self.list[row][i] * other.list[i][col] + each(i + 1)
+                if col == other.dim[1]:
+                    return []
+                return [each()] + across(col + 1)
+            if row == self.dim[0]:
+                return []
+            return [across()] + entire(row + 1)
+        return matrix(entire())
+
+    def scalar(self, scalar):
+        return matrix([[elem * scalar for elem in row] for row in self.list])
+        
+
+    def transpose(self):
+        """
+        transposes the matrix
+        """
+        if self.list != [] and self.list != [[]]:
+            outmat = []
+            for coli in range(self.dim[1]):
+                newrow = []
+                for rowi in range(self.dim[0]):
+                    newrow += [self.list[rowi][coli]]
+                outmat += [newrow]
+            return matrix(outmat)
+
+    def rotate(self, how_many_times = 1):
+        """
+        rotates a matrix (arg1) a number of times (arg2):
+        """
+        how_many_times %= 4
+        matter = self.list
+        while 0 < how_many_times:
+            how_many_times -= 1
+            new_mat = []
+            i = len(matter[0])
+            while i > 0:
+                i -= 1
+
+                row = []
+                for j in range(len(matter)):
+                    row += [matter[j][i]]
+                new_mat += [row]
+            matter = new_mat
+        return matrix(matter)
+    
+    def reflectVert(self):
+        return self.rotate(-1).transpose()
+
+    def reflectHoriz(self):
+        return self.rotate(1).transpose()
+
+    def printAlign(self, align="center"):
+        self.align = align
+
+
+
 
 
 
 def lrand():
     return int(random.random() * 10)
-# test = [
-#     [
-#         [0,0],
-#         [0,1],
-#     ],
-#     [
-#         [0,0,1,1],
-#         [0,1,0,1]
-#     ]
-# ]
-test = [[[lrand() for i in (0,1,2)] for j in (0,1)] for k in (0,1)]
-print_mat(test[0])
+
+test = [matrix([[lrand() for i in (0,1,2)] for j in (0,1)]) for k in (0,1)]
+print(test[0])
 print("~")
-print_mat(test[1])
+print(test[0].scalar(2))
 print("~")
-print_mat(add(test[0],test[1]))
+print(test[1])
 print("~")
-print_mat(transpose(add(test[0],test[1])))
-
-
-
-# mat_out = rotate(mat_in,1)
-
-# print(mat_in)
-# print(mat_out)
-
-# print("\n  ~  ~~ ~~~ ~~  ~  \n")
-
-# print_mat([[0,1]])
-# print("")
-# print_mat(mat_in)
-# print("")
-# print_mat(mat_out)
-
-# print("\n  ~  ~~ ~~~ ~~  ~  \n")
-
-# print_mat(mat_test)
-# print("")
-# print_mat(rotate(mat_test,-1))
-# print("")
-# print_mat(mat_test)
+print(test[0] * test[1])
+print("~")
+print(test[1] * test[0])
